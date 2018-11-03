59aeda0a58c60a62691dc3422d12b84319a88eea
venkatamusini <venkata.musini@gmail.com>
2018-11-03 11:15:25 -0400

    updated the values

--- sample_python/test_python.py
+++ sample_python/test_python.py
@@ -1,8 +1,19 @@
 from __future__ import print_function
 
-name = 'Cricket'
+my_fav_players = ['sachin', 'kohli', 'Dhoni', 'Prithvi shaw']
 
-if name == 'Sachin':
-    print('Very Good Player')
-else:
-    print('My Name is Kohli')
+for name in my_fav_players:
+    if name == 'sachin':
+        print('scored 100 centuries')
+    elif name == 'kohli':
+        print('current captain of indian team and on the way to score 100 centuries')
+    elif name == 'Dhoni':
+        print('Great pplayer and Human Being')
+    else:
+        print('Emerging player')
+        
+if __name__ == '__main__':
+    print(my_fav_players[1])
+        
+        
+            
